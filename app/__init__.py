b326fb6662b833d4fe123bc242fcba41ea85082b
fander2468 <86483561+fander2468@users.noreply.github.com>
2021-08-04 18:33:44 -0400

    segmented login page for security and created a registration page for users to register their credentialsp

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,21 @@
 from flask import Flask
 from config import Config
+# Import flask wrappers
+from flask_login import LoginManager # for logging users in and maintaining a sesion
+from flask_sqlalchemy import SQLAlchemy # This is used to talk to our database
+from flask_migrate import Migrate # Makes altering the Database a lot easier
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+# init Login Manager
+login = LoginManager(app) # used to login users and create a session
+login.login_view = 'login' # Used to redirect users if they enter wrong credentials or they are an invalid user
+
+# init the database from_object
+db = SQLAlchemy(app) # create a variable to talk to db and pass in the flask app 
+
+# init Migrate
+migrate = Migrate(app, db)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,28 @@
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 import requests
 from app import app
-from .forms import LoginForm
+from .forms import LoginForm, RegisterForm
+from .models import User
+from flask_login import login_user, logout_user, current_user, login_required
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        try:
+            new_user_data = {
+                'first_name' : form.first_name.data.title(),
+                'last_name' : form.last_name.data.title(),
+                'email' : form.email.data.lower(),
+                'password' : form.password.data
+            }
+            new_user_object = User()
+            new_user_object.from_dict(new_user_data)
+        except:
+            error_string = "There was a problem creating your account, please try again"
+            return render_template('register.html.j2', form=form, error = error_string)
+        return redirect(url_for('login'))
+    return render_template('register.html.j2', form=form)
 
 
 # used to display the login page - all the routes below
@@ -11,17 +32,37 @@ def login():
     if request.method == 'POST' and form.validate_on_submit():
         email = form.email.data.lower()
         password = form.password.data
-        if email in app.config.get('REGISTERED_USERS', {}).keys() and\
-             password == app.config.get('REGISTERED_USERS', {}).get(email).get('password'):
-             return f"Login was successful, Welcome {app.config.get('REGISTERED_USERS', {}).get(email).get('name')}"
-        error_string = "Incorrect Email/Password"
+        print('Here')
+        print(password)
+        user_account = User.query.filter_by(email=email).first()
+        print(user_account)
+        if user_account is not None and user_account.check_hashed_password(password):
+            login_user(user_account)
+            return redirect(url_for('pokemon'))
+        else:
+            return redirect(url_for('login'))
+
+        # if email in app.config.get('REGISTERED_USERS', {}).keys() and\
+        #      password == app.config.get('REGISTERED_USERS', {}).get(email).get('password'):
+        #      return f"Login was successful, Welcome {app.config.get('REGISTERED_USERS', {}).get(email).get('name')}"
+        # error_string = "Incorrect Email/Password"
         return render_template("index.html.j2", form=form, error=error_string)
     return render_template("index.html.j2", form=form)
         
 
 
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required        
+def logout():
+    if current_user:
+        logout_user()
+        return redirect(url_for('login'))
+
+
+
 # used to display pokemon data
 @app.route('/pokemon', methods=['GET', 'POST'])
+@login_required
 def pokemon():
     if request.method == 'POST':
         name = request.form.get('name')
